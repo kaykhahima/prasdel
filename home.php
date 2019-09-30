1e3b786e9155481b238ca0c57c8d920424d4adff
Kay Khahima <42249295+kaykhahima@users.noreply.github.com>
2019-09-30 20:59:01 +0300

    cc

--- home.php
+++ home.php
@@ -103,7 +103,7 @@
                         <div class="contents">
                             <h2 class="head-title mt-2" style="color:#d9aa3a;">Are you seeking <br> for an internship <br> or volunteering? <br> Need an IPT <br>placement?</h2>
                             <div class="welcome-text">
-                                <h2 class="head-title">Are you seeking for an internship or <br> volunteering? Need an IPT placement?</h2>
+                                <h2 class="head-title"  style="color:#d9aa3a;">Are you seeking for an internship or <br> volunteering? Need an IPT placement?</h2>
                             </div>
 
                         </div>
