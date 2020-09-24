eef7f55490c1490c848001a9a3d28f1ffbefda52
Kay Khahima <42249295+kaykhahima@users.noreply.github.com>
2020-09-24 15:51:23 +0300

    online course page

---
+++ build/online-courses.php
@@ -0,0 +1,52 @@
+ <?php
+$pageTitle = "Online Courses";
+include 'header.php';?>
+ <!-- Header Section End -->
+
+ <!-- Page Header Start -->
+ <div class="page-header">
+     <div class="container">
+         <div class="row">
+             <div class="col-lg-12">
+                 <div class="inner-header">
+                     <h3>Online Courses</h3>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+ <!-- Page Header End -->
+
+ <!-- Start Pricing Table Section -->
+ <div id="online-courses" class="section bg-gray">
+     <div class="container pb-5">
+         <div class="row">
+             <div class="col-md-4 pb-4">
+                 <div class="card">
+                     <img src="https://via.placeholder.com/400x240" class="card-img-top img-fluid" alt="...">
+                     <div class="card-body">
+                         <p class="pb-2"><a href="#"><span class="badge badge-primary">Development</span></a></p>
+                         <h4 class="card-title text-dark"><a href="course-1">Web Development with Laravel</a></h4>
+                         <p class="card-text font-weight-bold"><i class="lni lni-user"></i> <a href="#" class="text-dark">Prof. John Doe</a></p>
+                         <p class="card-text">If you are a web developer or web designer, this course is ideal for you. You can learn Laravel PHP Framework with this course.</p>
+                         <div class="row py-1">
+                             <div class="col-md-6">
+                                 <p><i class="lni lni-timer"></i> 2 weeks</p>
+                             </div>
+                             <div class="col-md-6">
+                                 <p><i class="lni lni-calendar"></i> 25/09/2020</p>
+                             </div>
+                             <div class="col-md-12 pt-3">
+                                 <a href="course-1" class="btn btn-common btn-block">Find out more</a>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+
+         </div>
+     </div>
+     <!-- End Pricing Table Section -->
+
+     <!-- Footer Section Start -->
+     <?php include 'footer.php';?>
